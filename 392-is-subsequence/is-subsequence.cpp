aa80627a18b8565cd7a71c54d1c1b687d381bdb9
Fatima86711 <fatimazafar411133@gmail.com>
2025-01-03 22:26:08 +0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (59.86%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        int j = 0;
+        if(n==0){
+            return true;
+        }
+        for(int i  = 0;i<m&&j<n;i++){
+            if(s[j]==t[i])
+            j++;
+            if(j==n){
+                return true;
+            }
+        }
+        return false;
+    }
+};
