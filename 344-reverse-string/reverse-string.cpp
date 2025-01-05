b589f0cef0c9a8c0c23a5a731c220b9661cc9489
Fatima86711 <fatimazafar411133@gmail.com>
2025-01-05 23:50:09 +0500

    Time: 0 ms (100.00%) | Memory: 27.1 MB (82.54%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
+        // int n = s.size();
         int k =0;
         int temp;
         int start = 0;
