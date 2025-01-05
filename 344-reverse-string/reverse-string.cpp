1fc06584a210506bd0dc648688b63ea608f7b722
Fatima86711 <fatimazafar411133@gmail.com>
2025-01-05 23:48:48 +0500

    Time: 0 ms (100.00%) | Memory: 27.3 MB (18.90%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int k =0;
+        int temp;
+        int start = 0;
+        int end= s.size()-1;
+       while(start<end){
+        temp= s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+       }
+    }
+};
