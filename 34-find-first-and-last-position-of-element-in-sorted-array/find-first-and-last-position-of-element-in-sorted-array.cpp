afaf81a3e277fe9e8a2b700de644b0c5ff0a780f
Fatima86711 <fatimazafar411133@gmail.com>
2025-01-14 22:28:23 +0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (72.30%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lb  = lowerBound(nums, target);
-        if(lb==nums.size()||nums[lb]!=target){
+        if(nums.empty()){
             return {-1,-1};
         }
-        return {lb, upperBound(nums, target)-1};
-    }
-    int lowerBound(vector<int>&arr, int target){
-    int n = arr.size();
-    int lower =0;
-    int k=n;
-    int mid;
-    int upper = n-1;
-    // int ans = n;
-    while(lower<=upper){
-         mid = (lower+upper)/2;
-        if(arr[mid]>=target){
-            k=mid;
-            upper = mid - 1;
+        if(firstPosition(nums, target)==-1){
+            return {-1,-1};
         }
         else{
-            lower = mid +1;
+            return {firstPosition(nums, target),lastPosition(nums, target)};
         }
-
     }
-    return k;
-}
-int upperBound(vector<int>&arr, int target){
-    int n = arr.size();
-    int lower =0;
-    int k=n;
-    int upper = n-1;
-    int mid;
-    // int ans = n;
-    while(lower<=upper){
-         mid = (lower+upper)/2;
-        if(arr[mid]>target){
-            k=mid;
-            upper = mid - 1;
+    int firstPosition(vector<int>&arr, int target){
+        int low = 0;
+        int high = arr.size()-1;
+        int first=-1;
+        while(high>=low){
+            int mid = (low+high)/2;
+            if(arr[mid]==target){
+                first = mid;
+                high =mid-1;
+            }
+            else if(arr[mid]<target){
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
         }
-        else{
-            lower = mid +1;
+        return first;
+    } 
+     int lastPosition(vector<int>&arr, int target){
+        int low = 0;
+        int high = arr.size()-1;
+        int last=-1;
+        while(high>=low){
+            int mid = (low+high)/2;
+            if(arr[mid]==target){
+                last = mid;
+                low =mid+1;
+            }
+            else if(arr[mid]<target){
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
         }
-
+        return last;
     }
-    return k;
-}
 };
