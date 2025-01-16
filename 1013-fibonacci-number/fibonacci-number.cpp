f64bf7e8c85973d248ce870f01e01774c94a8c83
Fatima86711 <fatimazafar411133@gmail.com>
2025-01-16 23:45:24 +0500

    Time: 8 ms (52.68%) | Memory: 7.8 MB (30.29%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,8 +4,6 @@ public:
         if(n<=1){
             return n;
         }
-        int first = fib(n-1);
-        int second = fib(n-2);
-        return first + second;
+        return fib(n-1) + fib(n-2);
     }
 };
