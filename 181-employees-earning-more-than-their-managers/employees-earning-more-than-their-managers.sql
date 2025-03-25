1898067b86ee05d46d9782e486a40810b4458295
Fatima86711 <fatimazafar411133@gmail.com>
2025-03-25 12:18:16 +0500

    Time: 456 ms (94.72%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,7 +1,3 @@
-/* Write your PL/SQL query statement below */
--- SELECT e2.name AS Employee FROM Employee e1
--- INNER JOIN Employee e2 ON e1.id = e2.managerID
--- WHERE e1.salary < e2.salary;
 
 SELECT e2.name as Employee
 FROM employee e1
