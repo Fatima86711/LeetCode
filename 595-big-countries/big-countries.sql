9e32b3cb95e7569f396437d2a5daa7d7b3fc5c41
Fatima86711 <fatimazafar411133@gmail.com>
2025-03-27 00:07:42 +0500

    Time: 318 ms (96.89%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,5 +1,5 @@
 /* Write your PL/SQL query statement below */
--- Write your PostgreSQL query statement below
-SELECT name , population , area 
-FROM World 
+
+SELECT name, population, area FROM WORLD
 WHERE area >= 3000000 OR population >= 25000000;
+
