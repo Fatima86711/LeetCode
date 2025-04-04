94de6f8e941552c5942a32124d5d29888bb2119a
Fatima86711 <fatimazafar411133@gmail.com>
2025-04-04 19:36:17 +0500

    Time: 651 ms (78.45%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,5 @@
 /* Write your PL/SQL query statement below */
-SELECT weather2.id FROM weather  weather2
-INNER JOIN weather  weather1
+SELECT weather2.id FROM weather  weather1
+INNER JOIN weather  weather2
 ON weather2.recordDate - weather1.recordDate =1
 AND weather2.temperature > weather1.temperature
