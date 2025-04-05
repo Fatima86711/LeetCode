c970a28fc0105309071f44aa0476665a34bc0eeb
Fatima86711 <fatimazafar411133@gmail.com>
2025-04-05 06:56:36 +0500

    Time: 316 ms (99.40%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,7 +1,7 @@
 SELECT a.machine_id, Round( AVG(b.timestamp-a.timestamp),3) as processing_time
 FROM Activity a
 JOIN Activity b
-ON a.process_id = b.process_id
-AND a.machine_id = b.machine_id
+ON a.machine_id = b.machine_id
+AND a.process_id = b.process_id 
 And a.timestamp<b.timestamp
 GROUP BY a.machine_id;
