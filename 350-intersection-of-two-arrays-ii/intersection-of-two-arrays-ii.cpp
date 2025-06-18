83c87a507984c87c643b31af9b052cec86977300
Fatima86711 <fatimazafar411133@gmail.com>
2025-06-18 06:17:27 +0500

    Time: 0 ms (100.00%) | Memory: 14.1 MB (89.18%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,26 +1,24 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> counts;
-        vector<int> result;
-
-        // Count the occurrences of each element in nums1
-        for (int num : nums1) {
-            counts[num]++;
-        }
-
-        // Find the intersection
-        for (int num : nums2) {
-            if (counts[num] > 0) {
-                result.push_back(num);
-                counts[num]--;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int p1=0, p2 = 0;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector <int> result;
+        while(p1< n1 && p2 < n2){
+            if(nums1[p1]<nums2[p2]){
+                p1++;
+            }
+            else if(nums1[p1]>nums2[p2]){
+                p2++;
+            }
+            else{
+                result.push_back(nums1[p1]);
+                p1++, p2++;
             }
         }
-
         return result;
     }
 };
