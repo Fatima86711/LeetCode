eae82d429fb556044a9069e1fc4d361b8ae4416d
Fatima86711 <fatimazafar411133@gmail.com>
2025-06-19 21:16:04 +0500

    Time: 4 ms (78.47%) | Memory: 14.6 MB (76.70%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        for (int i = 0; i < s.size(); i++) {
-            bool unique = true;  // Assume the character is unique
-            for (int j = 0; j < s.size(); j++) {
-                if (i != j && s[i] == s[j]) {  // Check for duplicates
-                    unique = false;
-                    break;
-                }
-            }
-            if (unique) {
-                return i;  // Return the index of the first unique character
+        vector <int> char_count(26,0);
+        for(char c:s){
+            char_count[c-'a']++;
+        }
+        for(int i = 0 ;i<s.size();i++){
+            int count = char_count[s[i]-'a'];
+            if(count==1){
+                return i;
             }
         }
-        return -1;  // Return -1 if no unique character is found
+        return -1;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         for (int i = 0; i < s.size(); i++) {
+//             bool unique = true;  
+//             for (int j = 0; j < s.size(); j++) {
+//                 if (i != j && s[i] == s[j]) { 
+//                     unique = false;
+//                     break;
+//                 }
+//             }
+//             if (unique) {
+//                 return i;  
+//             }
+//         }
+//         return -1;  
+//     }
+// };
