29ec5fa2bfd17e6ea22623c04ec924cdc62df241
Fatima86711 <fatimazafar411133@gmail.com>
2025-06-29 06:32:27 +0500

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.20%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -24,7 +24,9 @@ public:
                 high = mid-1;
 
             }
-            else{low = mid+1;
+            else if((mid%2)==0 && (nums[mid]== nums[mid+1])
+            ||
+            ((mid%2)==1)&&(nums[mid]==nums[mid-1])){low = mid+1;
             }
         }
         return -1;
