6a9d87ebf365c335de5739c29b64bef04e5a07cd
Fatima86711 <fatimazafar411133@gmail.com>
2025-07-04 12:58:57 +0500

    Time: 50 ms (34.11%) | Memory: 75.8 MB (22.61%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode*MergeTwoLists(ListNode*list1, ListNode*list2){
+ListNode*temp;
+    ListNode*dummy= new ListNode(-1);
+    temp = dummy;
+    
+    ListNode *t1= list1;
+    ListNode*t2 = list2;
+    while(t1!=NULL && t2!=NULL){
+        if(t1->val <t2->val){
+        temp->next=t1;
+        temp=t1;
+        t1=t1->next;
+    }
+    else{
+        temp->next=t2;
+        temp=t2;
+        t2=t2->next;
+    }
+
+
+    }
+    if(t1){
+        temp->next=t1;
+    }
+    else{
+        temp->next=t2;
+    }
+    
+    return dummy->next;
+}
+ListNode* FindMiddle(ListNode*head){
+    ListNode* fast = head->next, *slow=head;
+    while(fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow= slow->next;
+
+    }
+    
+    return slow;
+}
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode*middle = FindMiddle(head);
+        ListNode*right = middle->next;
+        middle->next=NULL;
+        ListNode*left = head;
+        left= sortList(left);
+        right=sortList(right);
+        return MergeTwoLists(left,right);
+    }
+    
+};
