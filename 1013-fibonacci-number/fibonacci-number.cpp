242f6505345d5a73b2f9973edcfb3010a6edd228
Fatima86711 <fatimazafar411133@gmail.com>
2025-07-25 06:15:20 +0500

    Time: 3 ms (66.67%) | Memory: 8.6 MB (11.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+long long fibonacci(int n , vector<long long>&memo ){
+    if(memo[n]!=-1){
+        return memo[n];
+    }
+    if(n<=1){
+        return n;
+    }
+    memo[n]= fibonacci(n-1, memo) + fibonacci(n-2, memo);
+    return memo[n];
+
+}
     int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-      return   fib(n-1) +fib(n-2);
+        vector<long long> memo(n+1, -1);
+return fibonacci(n, memo);
     }
 };
