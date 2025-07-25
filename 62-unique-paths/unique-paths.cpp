111a5a7bdd68ea0d3d922ed377f563499737432d
Fatima86711 <fatimazafar411133@gmail.com>
2025-07-25 07:50:14 +0500

    Time: 2 ms (18.58%) | Memory: 9.3 MB (57.59%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-long long uniquePathsUtil(int m, int n, vector<vector<long long>> &memo ){
-    if(m==1|| n==1 ){
-        return 1;
-    }
-    if(memo[m][n]==0)
-    memo[m][n] = uniquePathsUtil(m-1,n, memo) + uniquePathsUtil(m,n-1, memo);
-    return memo[m][n];
-}
     int uniquePaths(int m, int n) {
-      vector<vector<long long>> memo(m+1, vector<long long>( n+1,0));
-
-
-    return uniquePathsUtil(m,n, memo);
+        vector<vector<long long>> tabulation (m, vector<long long>(n, -1));
+        for(int i = 0;i<n;i++){
+        tabulation[0][i] = 1;
+        }
+        for(int i = 0;i<m;i++){
+        tabulation[i][0]=1;
+        }
+        for(int i =1;i<m;i++){
+            for(int k = 1;k<n;k++){
+                tabulation[i][k] = tabulation[i-1][k] + tabulation[i][k-1];
+            }
+        }
+        return tabulation[m-1][n-1];
     }
 };
