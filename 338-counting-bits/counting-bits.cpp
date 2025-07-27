72027f3ac2c6e3c26de035b7acef0a2ecf18b37e
Fatima86711 <fatimazafar411133@gmail.com>
2025-07-27 05:37:09 +0500

    Time: 0 ms (100.00%) | Memory: 10.6 MB (80.64%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n+1,0);
-        
-        for(int i =0 ;i<=n;i++){
-             int num = i;
-            int r=0;
-            while(num){
-                r+= num%2;
-                num=num/2;
-            }
-            ans[i]=r;
+        vector<int> ans(n+1, 0);
+        for(int i  = 0;i<=n;i++){
+            if(i%2==0)
+            ans[i] = ans[i/2];
+            else
+            ans[i] = (ans[i/2])+1;
         }
         return ans;
     }
