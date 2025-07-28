cd30ca2b7443741c1a1923d432d3f92b92d0d0a7
Fatima86711 <fatimazafar411133@gmail.com>
2025-07-28 05:38:01 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.69%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -2,7 +2,13 @@ class Solution {
 public:
 
     int tribonacci(int n) {
-        vector<long> tabulation(40,0);
+        vector<long> tabulation(n+1,0);
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
         tabulation[0] = 0;
         tabulation[1] = 1;
         tabulation[2] = 1;
